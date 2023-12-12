824df4fa95f1c138452ae0a0cc008975858a6e56
Kith933UTH <nguyenkiet51569@gmail.com>
2023-12-12 17:43:48 +0700

    Refactor: Route, Controller

--- src/index.js
+++ src/index.js
@@ -4,17 +4,24 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 app.use(morgan("combined"));
 app.use(express.static(__dirname + "/public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // Template engine
 app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//Routes
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
